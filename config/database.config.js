874c8a81a7658dc0264d79398d3ded341cfcdba5
peterleist <peterle606@gmail.com>
2021-03-30 17:29:45 +0200

    Add valid atlas db connection

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: 'mongodb://localhost:27017/easy-notes'
+    url: 'mongodb://admin:cYA9MNotsKhp9Pex@cluster0-shard-00-00.wleso.mongodb.net:27017,cluster0-shard-00-01.wleso.mongodb.net:27017,cluster0-shard-00-02.wleso.mongodb.net:27017/notesDB?ssl=true&replicaSet=atlas-d516la-shard-0&authSource=admin&retryWrites=true&w=majority'
 }

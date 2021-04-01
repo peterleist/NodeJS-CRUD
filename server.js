a084404b61598ab247f6f083612b42f2a82bf492
peterleist <peterle606@gmail.com>
2021-04-01 16:20:42 +0200

    Remove unused routes.

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 mongoose.connect(dbConfig.url, {
-	useMongoClient: true
+    useMongoClient: true
 });
 
 mongoose.connection.on('error', function() {
@@ -29,14 +29,13 @@ mongoose.connection.once('open', function() {
 })
 
 // define a simple route
-app.get('/', function(req, res){
-    res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
+app.get('/', function(req, res) {
+    res.json({ "message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes." });
 });
 
 require('./app/routes/note.routes.js')(app);
-require('./app/routes/location.routes.js')(app);
 
 // listen for requests
-app.listen(3000, function(){
+app.listen(3000, function() {
     console.log("Server is listening on port 3000");
 });
